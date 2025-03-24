555b9c436898b8d0f6320d0e98e5f3e58d172347
Charu Bohra <charu@juddoc.com>
2025-03-24 16:53:30 +0000

    Finish Logging

--- blango/settings.py
+++ blango/settings.py
@@ -15,6 +15,7 @@ import os
 from configurations import Configuration
 from configurations import values
 import dj_database_url
+import logging
 
 
 class Dev(Configuration): 
@@ -118,6 +119,44 @@ class Dev(Configuration):
         },
     ]
 
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "filters": {
+            "require_debug_false": {
+                "()": "django.utils.log.RequireDebugFalse",
+            },
+        },
+        "formatters": {
+            "verbose": {
+                "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+                "style": "{",
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                "formatter": "verbose",
+            },
+            "mail_admins": {
+                "level": "ERROR",
+                "class": "django.utils.log.AdminEmailHandler",
+                "filters": ["require_debug_false"],
+            },
+        },
+        "loggers": {
+            "django.request": {
+                "handlers": ["mail_admins"],
+                "level": "ERROR",
+                "propagate": True,
+            },
+        },
+        "root": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    }
 
     # Internationalization
     # https://docs.djangoproject.com/en/3.2/topics/i18n/
@@ -143,6 +182,8 @@ class Dev(Configuration):
     # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
     DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+    
 
 class Prod(Dev):
     DEBUG =False
--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render,get_object_or_404,redirect
 from django.utils import timezone
 from blog.models import Post
 from blog.forms import CommentForm
+import logging
+
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 def index(request):
   posts = Post.objects.filter(published_at__lte=timezone.now())
+  #log the number of posts at debug level
+  logger.debug("Got %d posts", len(posts))
   return render(request,"blog/index.html",{'posts':posts})
 
 def post_detail(request,slug):
@@ -24,5 +29,6 @@ def post_detail(request,slug):
       comment_form = CommentForm()
   else:
       comment_form = None
+  logger.info("Created comment on Post %d for user %s", post.pk, request.user)
   print("DEBUG: Comment form:", comment_form)  # Debugging output
   return render(request,"blog/post-detail.html" , {"post":post , "comment_form":comment_form})
